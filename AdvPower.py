d1f4188cb94429a77cc0124b9dd7e7ffeba4508e
macchiato <noun44963@gmail.com>
2025-09-03 21:16:27 -0400

    Update AdvPower.py

--- AdvPower.py
+++ AdvPower.py
@@ -2,19 +2,21 @@ import os
 import tkinter
 from tkinter import ttk
 
-def work():
+# Commands
+def placeholder():
     print("it work")
 
+# Window Management
 adv = tkinter.Tk()
 options = ttk.Frame(adv, padding=20)
 tkinter.Label(options, text="AdvPower", font="arial 15 bold").pack()
 ttk.Label(options, text="/// made by macchiato ///", font="arial 8 italic").pack()
-ttk.Button(options, text="Lock", command=work).pack()
-ttk.Button(options, text="Shutdown", command=work).pack()
-ttk.Button(options, text="Hibernate", command=work).pack()
-ttk.Button(options, text="Restart", command=work).pack()
-ttk.Button(options, text="Restart to Advanced Startup", command=work).pack()
-ttk.Button(options, text="Restart to BIOS", command=work).pack()
+ttk.Button(options, text="Lock", command=placeholder).pack()
+ttk.Button(options, text="Shutdown", command=placeholder).pack()
+ttk.Button(options, text="Hibernate", command=placeholder).pack()
+ttk.Button(options, text="Restart", command=placeholder).pack()
+ttk.Button(options, text="Restart to Advanced Startup", command=placeholder).pack()
+ttk.Button(options, text="Restart to BIOS", command=placeholder).pack()
 adv.resizable(False, False)
 adv.title("AdvPower")
 adv.maxsize(450, 450)
