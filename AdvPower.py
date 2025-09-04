1708c1ab65bdd1953b0a851840b95e2919dc3dbf
macchiato <noun44963@gmail.com>
2025-09-04 19:37:04 -0400

    Update AdvPower.py

--- AdvPower.py
+++ AdvPower.py
@@ -1,19 +1,24 @@
 import os
 import tkinter
 from tkinter import ttk
+from tkinter import messagebox
 
 # Commands
+def hibernate():
+    tkinter.messagebox.showinfo(title="AdvPower", message="Hibernate currently doesn't work")
+
 def placeholder():
-    print("it work")
+    tkinter.messagebox.showinfo(title="Works", message="it work")
+
 
 # Window Management
 adv = tkinter.Tk()
 options = ttk.Frame(adv, padding=20)
 tkinter.Label(options, text="AdvPower", font="arial 15 bold").pack()
 ttk.Label(options, text="/// made by macchiato ///", font="arial 8 italic").pack()
-ttk.Button(options, text="Lock", command=placeholder).pack()
+ttk.Button(options, text="Sign Out", command=placeholder).pack()
 ttk.Button(options, text="Shutdown", command=placeholder).pack()
-ttk.Button(options, text="Hibernate", command=placeholder).pack()
+ttk.Button(options, text="Hibernate", command=hibernate).pack()
 ttk.Button(options, text="Restart", command=placeholder).pack()
 ttk.Button(options, text="Restart to Advanced Startup", command=placeholder).pack()
 ttk.Button(options, text="Restart to BIOS", command=placeholder).pack()
@@ -22,3 +27,4 @@ adv.title("AdvPower")
 adv.maxsize(450, 450)
 options.pack()
 adv.mainloop()
+
