da4a54f43ce59763b1a698c6f617fdee1888602d
macchiato <noun44963@gmail.com>
2025-11-04 15:54:55 -0500

    edit the py file

--- AdvPower.py
+++ AdvPower.py
@@ -4,9 +4,6 @@ from tkinter import ttk
 from tkinter import messagebox
 
 # Commands
-def hibernate():
-    tkinter.messagebox.showinfo(title="AdvPower", message="Hibernate currently doesn't work")
-
 def placeholder():
     tkinter.messagebox.showinfo(title="Works", message="it work")
 
@@ -16,15 +13,14 @@ adv = tkinter.Tk()
 options = ttk.Frame(adv, padding=20)
 tkinter.Label(options, text="AdvPower", font="arial 15 bold").pack()
 ttk.Label(options, text="/// made by macchiato ///", font="arial 8 italic").pack()
-ttk.Button(options, text="Sign Out", command=placeholder).pack()
-ttk.Button(options, text="Shutdown", command=placeholder).pack()
-ttk.Button(options, text="Hibernate", command=hibernate).pack()
-ttk.Button(options, text="Restart", command=placeholder).pack()
-ttk.Button(options, text="Restart to Advanced Startup", command=placeholder).pack()
-ttk.Button(options, text="Restart to BIOS", command=placeholder).pack()
+ttk.Button(options, text="Sign Out", command=os.system("shutdown /l")).pack()
+ttk.Button(options, text="Shutdown", command=os.system("shutdown")).pack()
+ttk.Button(options, text="Hibernate", command=os.system("shutdown /h")).pack()
+ttk.Button(options, text="Restart", command=os.system("shutdown /r")).pack()
+ttk.Button(options, text="Restart to Advanced Startup", command=os.system("shutdown /r /o")).pack()
+ttk.Button(options, text="Restart to BIOS", command=os.system("shutdown /r /fw")).pack()
 adv.resizable(False, False)
 adv.title("AdvPower")
 adv.maxsize(450, 450)
 options.pack()
 adv.mainloop()
-
